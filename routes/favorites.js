fa4241a61a0a3140dcae1871ab9b0ffe3ee531b8
lechavezl <luisernestochavezleal@gmail.com>
2024-02-10 22:38:24 -0400

    Fixed favorite routes using the correct controller

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const propertiesController = require('../controllers/users');
+const propertiesController = require('../controllers/favorites');
 // const validation = require('../middleware/validate');
 // const { } = require('../middleware/authenticate');
 
 router.get('/', propertiesController.getAll);
 router.get('/:id', propertiesController.getSingle);
 
-router.post('/', propertiesController.createUser);
+router.post('/', propertiesController.createFavorite);
 
-router.put('/:id', propertiesController.updateUser);
+router.put('/:id', propertiesController.updateFavorite);
 
-router.delete('/:id', propertiesController.deleteUser);
+router.delete('/:id', propertiesController.deleteFavorite);
 module.exports = router;
